211746d767638ee61a0117082251765ca9114e32
Yuki326 <s20a1035@gmail.com>
2022-02-05 23:30:05 +0900

    必要なデータベースの追加

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.forms import IntegerField
+from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+  code = models.CharField(max_length=4)
+  num = models.IntegerField()
+  start = models.DateTimeField(blank=True,null=True)
+
+
+class Guess(models.Model):
+  room_id = models.ForeignKey(Room,on_delete=models.CASCADE)
+  info = models.CharField(max_length=4)
+  eat = models.IntegerField()
+  bite = models.IntegerField()
+  created_at = models.DateTimeField()
